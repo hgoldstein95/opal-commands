9eeed45ef125652a039d69190e67f5e48774ebfa
Harrison Goldstein <hgoldstein95@gmail.com>
2017-02-02 10:11:23 -0500

    Working on making VW interact with the TS data
    There should probably be a better way to make that happen. Data should be easily
    serializable

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
-function greeter(person: string) {
-    return "Hello, " + person + "!";
+import * as child from 'child_process';
+
+class CommandFeature {
+    f1: number;
+    f2: number;
+    constructor(f1: number, f2: number) {
+        this.f1 = f1;
+        this.f2 = f2;
+    }
+
+    toWabbit() {
+        return `| f1:${this.f1} f2:${this.f2}`;
+    }
 }
 
-let user = "Harry";
+class Wabbit {
+    train(data: CommandFeature[]) {
+    }
 
-console.log(greeter(user));
+    predict(data: CommandFeature) {
+        console.log(data.toWabbit());
+
+        child.exec("ls", (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+        });
+    }
+}
+
+let wabbit = new Wabbit();
+
+wabbit.predict(new CommandFeature(10, 20));
