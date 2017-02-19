9a393ded852c78d9540ff15f7600e0491267c027
Harrison Goldstein <hgoldstein95@gmail.com>
2017-02-18 22:32:58 -0500

    Added some wyatt-ey examples
    Also added a theory document with some interesting formalisms

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import { densify } from './densify';
 
 // --- Examples ---
 
+/*
 let f1 = new BagOfWordsFeature("f1", "bar", ["foo", "bar", "baz"]);
 let f2 = new NumberFeature("f2", 42);
 let f3 = new BooleanFeature("f3", false);
@@ -26,3 +27,31 @@ console.log(densify(fvec));
 console.log(densify(gvec));
 console.log(distance(fvec, gvec));
 console.log(distance([f1, f2, f3, f4], [g1, g2, g3, g4]));
+*/
+
+let v1 = {
+    tag: "ls",
+    vec: [
+        new SparseFeature("dir", "/home/harry"),
+        new NumberFeature("hist", 2),
+        new BooleanFeature("success", true)
+    ]
+};
+let v2 = {
+    tag: "npm",
+    vec: [
+        new SparseFeature("dir", "/home/harry/Projects/wyatt"),
+        new NumberFeature("hist", 1),
+        new BooleanFeature("success", true)
+    ]
+};
+let v3 = {
+    tag: "ls",
+    vec: [
+        new SparseFeature("dir", "/home/harry/Projects/wyatt"),
+        new NumberFeature("hist", 0),
+        new BooleanFeature("success", true)
+    ]
+};
+console.log(distance(v1.vec, v2.vec));
+console.log(distance(v3.vec, v2.vec));
