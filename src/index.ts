6abf4b81948a40128f38a7adfa09f2811ce601d1
Harrison Goldstein <hgoldstein95@gmail.com>
2017-02-18 22:51:27 -0500

    Updated npm scripts, added tests, and other fun stuff

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,6 @@ import {
 import { distance } from './distance';
 import { densify } from './densify';
 
-// --- Examples ---
-
-/*
-let f1 = new BagOfWordsFeature("f1", "bar", ["foo", "bar", "baz"]);
-let f2 = new NumberFeature("f2", 42);
-let f3 = new BooleanFeature("f3", false);
-let f4 = new SparseFeature("f4", "/home/harry");
-
-let g1 = new BagOfWordsFeature("g1", "foo", ["foo", "bar", "baz"]);
-let g2 = new NumberFeature("g2", 41);
-let g3 = new BooleanFeature("g3", true);
-let g4 = new SparseFeature("g4", "/home/harry");
-
-let fvec = [f1, f2, f3];
-let gvec = [g1, g2, g3];
-
-console.log(densify(fvec));
-console.log(densify(gvec));
-console.log(distance(fvec, gvec));
-console.log(distance([f1, f2, f3, f4], [g1, g2, g3, g4]));
-*/
-
 let v1 = {
     tag: "ls",
     vec: [
---
+++ test/test.ts
@@ -0,0 +1,42 @@
+import * as test from 'tape';
+import {
+    NumberFeature,
+    BooleanFeature,
+    BagOfWordsFeature,
+    SparseFeature
+} from '../src/features';
+import { densify } from '../src/densify';
+import { distance } from '../src/distance';
+
+let f1 = new BagOfWordsFeature("f1", "bar", ["foo", "bar", "baz"]);
+let f2 = new NumberFeature("f2", 42);
+let f3 = new BooleanFeature("f3", false);
+let f4 = new SparseFeature("f4", "/home/harry");
+
+let g1 = new BagOfWordsFeature("g1", "foo", ["foo", "bar", "baz"]);
+let g2 = new NumberFeature("g2", 41);
+let g3 = new BooleanFeature("g3", true);
+let g4 = new SparseFeature("g4", "/home/harry");
+
+let fvec = [f1, f2, f3];
+let gvec = [g1, g2, g3];
+
+test('densify_1', (t) => {
+    t.same(densify(fvec), [0, 1, 0, 42, 0]);
+    t.end();
+});
+
+test('densify_2', (t) => {
+    t.same(densify(gvec), [1, 0, 0, 41, 1]);
+    t.end();
+});
+
+test('distance_1', (t) => {
+    t.equal(distance(fvec, gvec), 2);
+    t.end();
+});
+
+test('distance_2', (t) => {
+    t.equal(distance([f1, f2, f3, f4], [g1, g2, g3, g4]), 2);
+    t.end();
+});
